2ae3081849da50000c11dde9c7dc9478b715d722
KhalilC <klcrumpler@gmail.com>
2015-10-19 20:37:23 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+
+def palindrome?(result)
+  result == result.to_s.reverse.to_i
+end
+
+def largest_palindrome_product
+  (100..999).map do |first_num|
+    (first_num..999).map do |second_num|
+      first_num * second_num
+     end.select {|results| palindrome?(results)}
+  end.flatten.max
+end
+
+
+
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,17 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+  
+  def palindrome?(result)
+  result == result.to_s.reverse.to_i
+  end
+
+  def answer
+    (100..999).map do |first_num|
+      (first_num..999).map do |second_num|
+        first_num * second_num
+       end.select {|results| palindrome?(results)}
+    end.flatten.max
+  end
+    
+end
